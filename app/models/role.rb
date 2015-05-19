436f6a9703091dce7deee9dc7de7b977b927bfe8
karinehoffman <khoffman2016@kellogg.northwestern.edu>
2015-05-18 20:58:16 -0500

    Update

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,17 @@
 class Role < ActiveRecord::Base
+
+  validates :character_name, :presence => true
+
+  belongs_to :actor, :class_name => "Actor", :foreign_key => "actor_id"
+
+  def actor
+    return Actor.find_by({ :id => self.actor_id})
+  end
+
+  belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
+
+  def movie
+    return Movie.find_by({ :id => self.movie_id})
+  end
+
 end
